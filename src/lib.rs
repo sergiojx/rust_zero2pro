969b5a7434e47c9a25aae22fffde4a5cebe72660
sacosta <sf_acosta@hotmail.com>
2024-06-06 18:45:08 +0000

    move HttpServer into a library

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+pub async fn fun() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+// use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+// async fn greet(req: HttpRequest) -> impl Responder {
+//     let name = req.match_info().get("name").unwrap_or("World");
+//     format!("Hello {}!", &name)
+// }
+
+// async fn health_check() -> impl Responder {
+//     HttpResponse::Ok()
+// }
+
+use zero2prod::fun;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("0.0.0.0:8080")?
-    .run()
-    .await
+    fun().await
 }
